fa5ae08ed712d5c4d2d74bbaa6bf83d483ffcddc
Okumans <kaopancraft@gmail.com>
2025-03-22 23:44:24 +0700

    feat: add UserSessionCard component for displaying user session details with edit and delete options

---
+++ src/app/_components/UserSessionCard.tsx
@@ -0,0 +1,51 @@
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/shadcn/dropdown-menu";
+import { Calendar, Phone } from "lucide-react";
+import { SessionCardInfo, SessionCardWithDropdown } from "./SessionCard";
+
+interface UserSessionCardProps {
+  title: string;
+  description: string;
+  tel: string;
+  date: string;
+
+  onEdit?: () => void;
+  onDelete?: () => void;
+}
+
+export const UserSessionCard: React.FC<UserSessionCardProps> = ({
+  title,
+  description,
+  tel,
+  date,
+  onEdit,
+  onDelete,
+}) => {
+  const dropdownContent = (
+    <DropdownMenuContent>
+      <DropdownMenuLabel>Interview Session</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem disabled={!onEdit} onClick={onEdit}>
+        Edit Session
+      </DropdownMenuItem>
+      <DropdownMenuItem disabled={!onDelete} onClick={onDelete}>
+        Delete Session
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  );
+
+  return (
+    <SessionCardWithDropdown
+      title={title}
+      description={description}
+      dropdownContent={dropdownContent}
+    >
+      <SessionCardInfo icon={<Phone />} text={tel} />
+      <SessionCardInfo icon={<Calendar />} text={date} />
+    </SessionCardWithDropdown>
+  );
+};
